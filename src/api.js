13b2e001209a8609ff9a0a3dec2d113e34f655b8
ruslanepi <ruslanep2@gmail.com>
2021-09-13 00:37:26 +0300

    added redux, data api

---
+++ src/api.js
@@ -0,0 +1,34 @@
+const base_url = 'https://api.rawg.io/api/'
+//getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1
+  if (month < 10) {
+    return `0${month}`
+  } else {
+    return month
+  }
+}
+const getCurrentDay = () => {
+  const day = new Date().getDate()
+  if (day < 10) {
+    return `0${day}`
+  } else {
+    return day
+  }
+}
+const currentYear = new Date().getFullYear()
+const currentMonth = getCurrentMonth()
+const currentDay = getCurrentDay()
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
+
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
+
+//popular games
+const popular_games = `games?key=b521818db2ba4cf1938be4360e1f5b26&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+const upcoming_games = `games?key=b521818db2ba4cf1938be4360e1f5b26&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
+const new_games = `games?key=b521818db2ba4cf1938be4360e1f5b26&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
+
+export const popularGamesURL = () => `${base_url}${popular_games}`
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`
+export const newGamesURL = () => `${base_url}${new_games}`
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './reducers'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
+//b521818db2ba4cf1938be4360e1f5b26
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
